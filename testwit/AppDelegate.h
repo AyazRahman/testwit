2233564cc54f0e92e241ad5cdfd096c6e73d5f5e
muhtasim3504 <muhtasim_ayaz@yahoo.com>
2014-10-12 12:18:38 -0700

    Initial Commit

---
+++ testwit/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  testwit
+//
+//  Created by Muhtasim Ayaz on 10/12/14.
+//  Copyright (c) 2014 Muhtasim Ayaz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ testwit/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  testwit
+//
+//  Created by Muhtasim Ayaz on 10/12/14.
+//  Copyright (c) 2014 Muhtasim Ayaz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
